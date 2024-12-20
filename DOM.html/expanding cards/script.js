eadce20724d0f5a9e5856f8fc9a0229bdb86a857
Sridhar7670 <140085461+Sridhar7670@users.noreply.github.com>
2024-12-20 10:02:06 +0530

    expanded cards
    expanding images by onclick event

---
+++ DOM.html/expanding cards/script.js
@@ -0,0 +1,12 @@
+
+function active(event){
+    let panels = document.querySelectorAll('.panel'); 
+    // Remove the "active","show" class from all panels
+
+    panels.forEach((panel) => panel.classList.remove('active'));
+    panels.forEach((show) => show.classList.remove('show'));
+    // Add the "active","show" class to the clicked panel
+
+    event.target.classList.add('active');
+    event.target.classList.add('show');
+}
